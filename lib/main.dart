19765adab264561539dc24cf6bcd927c36686ad0
tegarjokam <dtegarjokam354@gmail.com>
2020-06-28 18:21:03 +0900

    add transaction model and add ui on the screen with corresponding transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './model/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,8 +16,62 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Laptop',
+      amount: 500.0,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'New T-shirt',
+      amount: 23.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'New Table',
+      amount: 499.99,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expense App"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text("Chart"),
+            ),
+          ),
+          Column(
+            children: transactions.map((t) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(t.amount.toString()),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(t.title),
+                        Text(t.date.toLocal().toString()),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+    );
   }
 }
